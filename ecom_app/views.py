2bc4ab30bfa6dc6895cf3bc10b8ed13c0330ebb7
Rahul kumar <rahul8969051051@gmail.com>
2023-12-21 14:33:50 +0530

    header dynamic

--- ecom_app/views.py
+++ ecom_app/views.py
@@ -21,7 +21,13 @@ except Exception as err:
 def index(request):
     template = loader.get_template("result_Home.html")
     # results = VisaGetlist.objects.all()
-    context = {'user': 'arun'}
+    try:
+        generated_org_id = request.session['generated_org_id']
+        json_result,json_result_1,item_rows,item_sub_row = check_generated_org_id(request)
+    except:
+        json_result,json_result_1,item_rows,item_sub_row  = None, None, None,None
+    print(json_result)
+    context = {'logo_url': json_result,'org_name': json_result_1,'item_rows':item_rows,"item_sub_row":item_sub_row }
     rendered_template = template.render(context)
     return HttpResponse(rendered_template)
 
@@ -87,11 +93,11 @@ def list_product(request):
     # results = VisaGetlist.objects.all()
     try:
         generated_org_id = request.session['generated_org_id']
-        json_result,json_result_1,item_rows = check_generated_org_id(request)
+        json_result,json_result_1,item_rows,item_sub_row = check_generated_org_id(request)
     except:
-        json_result,json_result_1,item_rows = None, None, None
+        json_result,json_result_1,item_rows,item_sub_row  = None, None, None,None
     print(json_result)
-    context = {'logo_url': json_result,'org_name': json_result_1,'item_rows':item_rows}
+    context = {'logo_url': json_result,'org_name': json_result_1,'item_rows':item_rows,"item_sub_row":item_sub_row }
     rendered_template = template.render(context)
     return HttpResponse(rendered_template)
 
@@ -103,11 +109,11 @@ def list_product_details(request):
     # results = VisaGetlist.objects.all()
     try:
         generated_org_id = request.session['generated_org_id']
-        json_result,json_result_1,item_rows = check_generated_org_id(request)
+        json_result,json_result_1,item_rows,item_sub_row = check_generated_org_id(request)
     except:
-        json_result,json_result_1,item_rows = None, None, None
+        json_result,json_result_1,item_rows,item_sub_row = None, None, None,None
     print(json_result)
-    context = {'logo_url': json_result,'org_name': json_result_1,'item_rows':item_rows}
+    context = {'logo_url': json_result,'org_name': json_result_1,'item_rows':item_rows,"item_sub_row":item_sub_row}
     rendered_template = template.render(context)
     return HttpResponse(rendered_template)
 
@@ -126,7 +132,7 @@ def check_generated_org_id(request):
 
     if not generated_org_id:
         # Handle case where generated_org_id is not found
-        return None, None, None
+        return None, None, None,None
 
     try:
         # SQL query for user_app_generate_org_id
@@ -147,30 +153,38 @@ def check_generated_org_id(request):
         item_query = item_query.bindparams(json_result_1=json_result_1)
         item_result = conn.execute(item_query).fetchall()
 
+        # SQL query for itemsubtype
+        item_sub_query = text("SELECT * FROM shivadb_new.itemsubtype WHERE org_id = :json_result_1")
+        item_sub_query = item_sub_query.bindparams(json_result_1=json_result_1)
+        item_sub_result = conn.execute(item_sub_query).fetchall()
+        print("items",item_sub_result)
+
         # Convert the results to lists of dictionaries
         user_rows = [dict(row._asdict()) for row in user_result]
         item_rows = [dict(row._asdict()) for row in item_result]
+        item_sub_row=[dict(row._asdict()) for row in item_sub_result]
+        print("item-sub", item_sub_row)
 
         # Convert the list of dictionaries to JSON
         json_result = "https://saasapps.in:2082/media/" + user_rows[0]['src']
 
-        return json_result, json_result_1, item_rows
+        return json_result, json_result_1, item_rows, item_sub_row
 
     except Exception as e:
         # Handle any exceptions that may occur during execution
         print(f"Error: {e}")
-        return None, None, None
+        return None, None, None,None
 
 def generated_org_id(request, generated_org_id):
     print("here")
     # Store generated_org_id in session
     request.session['generated_org_id'] = generated_org_id
     try:
-        json_result,json_result_1,item_rows = check_generated_org_id(request)
+        json_result,json_result_1,item_rows,item_sub_row = check_generated_org_id(request)
     except:
-        json_result,json_result_1,item_rows = None, None, None
+        json_result,json_result_1,item_rows,item_sub_row = None, None, None,None
     print(json_result)
-    context = {'logo_url': json_result,'org_name': json_result_1,'item_rows':item_rows}
+    context = {'logo_url': json_result,'org_name': json_result_1,'item_rows':item_rows,"item_sub_row":item_sub_row}
     # Redirect to the home page or any other page you desire
     return render(request, 'result_Home.html', context)
 
