a398dedfca38db2ca9c7a0a5792e8d14d827b390
arun12341234 <ak080495@gmail.com>
2023-12-22 20:43:35 +0530

    setting *

--- ecom_shop/settings.py
+++ ecom_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d!3aadh*^)tnthtkh^d+vbokp!bwa&u@=6*g*qjz#@$o^wobd-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['saasapps.in','127.0.0.1','49.205.199.235']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
